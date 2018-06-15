e6a69200a78caafb34f9e39dcd3e4a8df23e4bc5
= <mbfrahry@gmail.com>
2018-06-15 11:24:20 -0600

    Fmting tests

--- oraclepaas/resource_java_access_rule_test.go
+++ oraclepaas/resource_java_access_rule_test.go
@@ -115,65 +115,65 @@ func testAccCheckJavaAccessRuleDestroy(s *terraform.State) error {
 func testAccJavaAccessRuleBasic(rInt int) string {
 	return fmt.Sprintf(`
 resource "oraclepaas_database_service_instance" "test" {
-	name        = "test-service-instance-%d"
-	description = "test service instance"
-	edition = "EE"
-	level = "PAAS"
-	shape = "oc3"
-	subscription_type = "HOURLY"
-	version = "12.2.0.1"
-	ssh_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC3QxPp0BFK+ligB9m1FBcFELyvN5EdNUoSwTCe4Zv2b51OIO6wGM/dvTr/yj2ltNA/Vzl9tqf9AUBL8tKjAOk8uukip6G7rfigby+MvoJ9A8N0AC2te3TI+XCfB5Ty2M2OmKJjPOPCd6+OdzhT4cWnPOM+OAiX0DP7WCkO4Kx2kntf8YeTEurTCspOrRjGdo+zZkJxEydMt31asu9zYOTLmZPwLCkhel8vY6SnZhDTNSNkRzxZFv+Mh2VGmqu4SSxfVXr4tcFM6/MbAXlkA8jo+vHpy5sC79T4uNaPu2D8Ed7uC3yDdO3KRVdzZCfWHj4NjixdMs2CtK6EmyeVOPuiYb8/mcTybrb4F/CqA4jydAU6Ok0j0bIqftLyxNgfS31hR1Y3/GNPzly4+uUIgZqmsuVFh5h0L7qc1jMv7wRHphogo5snIp45t9jWNj8uDGzQgWvgbFP5wR7Nt6eS0kaCeGQbxWBDYfjQE801IrwhgMfmdmGw7FFveCH0tFcPm6td/8kMSyg/OewczZN3T62ETQYVsExOxEQl2t4SZ/yqklg+D9oGM+ILTmBRzIQ2m/xMmsbowiTXymjgVmvrWuc638X6dU2fKJ7As4hxs3rA1BA5sOt0XyqfHQhtYrL/Ovb1iV+C7MRhKicTyoNTc7oVcDDG0VW785d8CPqttDi50w=="
-	bring_your_own_license = true
+    name        = "test-service-instance-%d"
+    description = "test service instance"
+    edition = "EE"
+    level = "PAAS"
+    shape = "oc3"
+    subscription_type = "HOURLY"
+    version = "12.2.0.1"
+    ssh_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC3QxPp0BFK+ligB9m1FBcFELyvN5EdNUoSwTCe4Zv2b51OIO6wGM/dvTr/yj2ltNA/Vzl9tqf9AUBL8tKjAOk8uukip6G7rfigby+MvoJ9A8N0AC2te3TI+XCfB5Ty2M2OmKJjPOPCd6+OdzhT4cWnPOM+OAiX0DP7WCkO4Kx2kntf8YeTEurTCspOrRjGdo+zZkJxEydMt31asu9zYOTLmZPwLCkhel8vY6SnZhDTNSNkRzxZFv+Mh2VGmqu4SSxfVXr4tcFM6/MbAXlkA8jo+vHpy5sC79T4uNaPu2D8Ed7uC3yDdO3KRVdzZCfWHj4NjixdMs2CtK6EmyeVOPuiYb8/mcTybrb4F/CqA4jydAU6Ok0j0bIqftLyxNgfS31hR1Y3/GNPzly4+uUIgZqmsuVFh5h0L7qc1jMv7wRHphogo5snIp45t9jWNj8uDGzQgWvgbFP5wR7Nt6eS0kaCeGQbxWBDYfjQE801IrwhgMfmdmGw7FFveCH0tFcPm6td/8kMSyg/OewczZN3T62ETQYVsExOxEQl2t4SZ/yqklg+D9oGM+ILTmBRzIQ2m/xMmsbowiTXymjgVmvrWuc638X6dU2fKJ7As4hxs3rA1BA5sOt0XyqfHQhtYrL/Ovb1iV+C7MRhKicTyoNTc7oVcDDG0VW785d8CPqttDi50w=="
+    bring_your_own_license = true
 
-	database_configuration {
-		admin_password = "Test_String7"
-		backup_destination = "OSS"
-		failover_database = false
-		sid = "ORCL"
-		usable_storage = 15
-	}
+    database_configuration {
+        admin_password = "Test_String7"
+        backup_destination = "OSS"
+        failover_database = false
+        sid = "ORCL"
+        usable_storage = 15
+    }
 
-	backups {
-		cloud_storage_container = "%sacctest-%d"
-		create_if_missing = true
-	}
+    backups {
+        cloud_storage_container = "%sacctest-%d"
+        create_if_missing = true
+    }
 }
 
 resource "oraclepaas_java_service_instance" "test" {
-	name = "tfinstance%d"
-	edition = "SUITE"
-	service_version = "12cRelease212"
-	ssh_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC3QxPp0BFK+ligB9m1FBcFELyvN5EdNUoSwTCe4Zv2b51OIO6wGM/dvTr/yj2ltNA/Vzl9tqf9AUBL8tKjAOk8uukip6G7rfigby+MvoJ9A8N0AC2te3TI+XCfB5Ty2M2OmKJjPOPCd6+OdzhT4cWnPOM+OAiX0DP7WCkO4Kx2kntf8YeTEurTCspOrRjGdo+zZkJxEydMt31asu9zYOTLmZPwLCkhel8vY6SnZhDTNSNkRzxZFv+Mh2VGmqu4SSxfVXr4tcFM6/MbAXlkA8jo+vHpy5sC79T4uNaPu2D8Ed7uC3yDdO3KRVdzZCfWHj4NjixdMs2CtK6EmyeVOPuiYb8/mcTybrb4F/CqA4jydAU6Ok0j0bIqftLyxNgfS31hR1Y3/GNPzly4+uUIgZqmsuVFh5h0L7qc1jMv7wRHphogo5snIp45t9jWNj8uDGzQgWvgbFP5wR7Nt6eS0kaCeGQbxWBDYfjQE801IrwhgMfmdmGw7FFveCH0tFcPm6td/8kMSyg/OewczZN3T62ETQYVsExOxEQl2t4SZ/yqklg+D9oGM+ILTmBRzIQ2m/xMmsbowiTXymjgVmvrWuc638X6dU2fKJ7As4hxs3rA1BA5sOt0XyqfHQhtYrL/Ovb1iV+C7MRhKicTyoNTc7oVcDDG0VW785d8CPqttDi50w=="
-	force_delete = true	
-	bring_your_own_license = true
+    name = "tfinstance%d"
+    edition = "SUITE"
+    service_version = "12cRelease212"
+    ssh_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC3QxPp0BFK+ligB9m1FBcFELyvN5EdNUoSwTCe4Zv2b51OIO6wGM/dvTr/yj2ltNA/Vzl9tqf9AUBL8tKjAOk8uukip6G7rfigby+MvoJ9A8N0AC2te3TI+XCfB5Ty2M2OmKJjPOPCd6+OdzhT4cWnPOM+OAiX0DP7WCkO4Kx2kntf8YeTEurTCspOrRjGdo+zZkJxEydMt31asu9zYOTLmZPwLCkhel8vY6SnZhDTNSNkRzxZFv+Mh2VGmqu4SSxfVXr4tcFM6/MbAXlkA8jo+vHpy5sC79T4uNaPu2D8Ed7uC3yDdO3KRVdzZCfWHj4NjixdMs2CtK6EmyeVOPuiYb8/mcTybrb4F/CqA4jydAU6Ok0j0bIqftLyxNgfS31hR1Y3/GNPzly4+uUIgZqmsuVFh5h0L7qc1jMv7wRHphogo5snIp45t9jWNj8uDGzQgWvgbFP5wR7Nt6eS0kaCeGQbxWBDYfjQE801IrwhgMfmdmGw7FFveCH0tFcPm6td/8kMSyg/OewczZN3T62ETQYVsExOxEQl2t4SZ/yqklg+D9oGM+ILTmBRzIQ2m/xMmsbowiTXymjgVmvrWuc638X6dU2fKJ7As4hxs3rA1BA5sOt0XyqfHQhtYrL/Ovb1iV+C7MRhKicTyoNTc7oVcDDG0VW785d8CPqttDi50w=="
+    force_delete = true	
+    bring_your_own_license = true
 
-	weblogic_server {
-		shape = "oc3"
-		database {
-			name = "${oraclepaas_database_service_instance.test.name}"
-			username = "sys"
-			password = "Test_String7"
+    weblogic_server {
+        shape = "oc3"
+        database {
+            name = "${oraclepaas_database_service_instance.test.name}"
+            username = "sys"
+            password = "Test_String7"
 		}
-		admin {
-			username = "terraform-user"
-			password = "Test_String7"
+        admin {
+            username = "terraform-user"
+            password = "Test_String7"
 		}
-	}
+    }
 
-	backups {
-		cloud_storage_container = "%sacctest-%d"
-		auto_generate = true
-	}
+    backups {
+        cloud_storage_container = "%sacctest-%d"
+        auto_generate = true
+    }
 }
 
 resource "oraclepaas_java_access_rule" "test" {
-	name = "test-access-rule-%d"
-	service_instance_id = "${oraclepaas_java_service_instance.test.name}"
-	description = "test-access-rule"
-	ports = "8000"
-	source = "PUBLIC-INTERNET"
-	destination = "WLS_ADMIN"
-	enabled = false
+    name = "test-access-rule-%d"
+    service_instance_id = "${oraclepaas_java_service_instance.test.name}"
+    description = "test-access-rule"
+    ports = "8000"
+    source = "PUBLIC-INTERNET"
+    destination = "WLS_ADMIN"
+    enabled = false
 }
 `, rInt, os.Getenv("OPC_STORAGE_URL"), rInt, rInt, os.Getenv("OPC_STORAGE_URL"), rInt, rInt)
 }
@@ -181,65 +181,65 @@ resource "oraclepaas_java_access_rule" "test" {
 func testAccJavaAccessRuleUpdate(rInt int) string {
 	return fmt.Sprintf(`
 resource "oraclepaas_database_service_instance" "test" {
-	name        = "test-service-instance-%d"
-	description = "test service instance"
-	edition = "EE"
-	level = "PAAS"
-	shape = "oc3"
-	subscription_type = "HOURLY"
-	version = "12.2.0.1"
-	ssh_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC3QxPp0BFK+ligB9m1FBcFELyvN5EdNUoSwTCe4Zv2b51OIO6wGM/dvTr/yj2ltNA/Vzl9tqf9AUBL8tKjAOk8uukip6G7rfigby+MvoJ9A8N0AC2te3TI+XCfB5Ty2M2OmKJjPOPCd6+OdzhT4cWnPOM+OAiX0DP7WCkO4Kx2kntf8YeTEurTCspOrRjGdo+zZkJxEydMt31asu9zYOTLmZPwLCkhel8vY6SnZhDTNSNkRzxZFv+Mh2VGmqu4SSxfVXr4tcFM6/MbAXlkA8jo+vHpy5sC79T4uNaPu2D8Ed7uC3yDdO3KRVdzZCfWHj4NjixdMs2CtK6EmyeVOPuiYb8/mcTybrb4F/CqA4jydAU6Ok0j0bIqftLyxNgfS31hR1Y3/GNPzly4+uUIgZqmsuVFh5h0L7qc1jMv7wRHphogo5snIp45t9jWNj8uDGzQgWvgbFP5wR7Nt6eS0kaCeGQbxWBDYfjQE801IrwhgMfmdmGw7FFveCH0tFcPm6td/8kMSyg/OewczZN3T62ETQYVsExOxEQl2t4SZ/yqklg+D9oGM+ILTmBRzIQ2m/xMmsbowiTXymjgVmvrWuc638X6dU2fKJ7As4hxs3rA1BA5sOt0XyqfHQhtYrL/Ovb1iV+C7MRhKicTyoNTc7oVcDDG0VW785d8CPqttDi50w=="
-	bring_your_own_license = true
+    name        = "test-service-instance-%d"
+    description = "test service instance"
+    edition = "EE"
+    level = "PAAS"
+    shape = "oc3"
+    subscription_type = "HOURLY"
+    version = "12.2.0.1"
+    ssh_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC3QxPp0BFK+ligB9m1FBcFELyvN5EdNUoSwTCe4Zv2b51OIO6wGM/dvTr/yj2ltNA/Vzl9tqf9AUBL8tKjAOk8uukip6G7rfigby+MvoJ9A8N0AC2te3TI+XCfB5Ty2M2OmKJjPOPCd6+OdzhT4cWnPOM+OAiX0DP7WCkO4Kx2kntf8YeTEurTCspOrRjGdo+zZkJxEydMt31asu9zYOTLmZPwLCkhel8vY6SnZhDTNSNkRzxZFv+Mh2VGmqu4SSxfVXr4tcFM6/MbAXlkA8jo+vHpy5sC79T4uNaPu2D8Ed7uC3yDdO3KRVdzZCfWHj4NjixdMs2CtK6EmyeVOPuiYb8/mcTybrb4F/CqA4jydAU6Ok0j0bIqftLyxNgfS31hR1Y3/GNPzly4+uUIgZqmsuVFh5h0L7qc1jMv7wRHphogo5snIp45t9jWNj8uDGzQgWvgbFP5wR7Nt6eS0kaCeGQbxWBDYfjQE801IrwhgMfmdmGw7FFveCH0tFcPm6td/8kMSyg/OewczZN3T62ETQYVsExOxEQl2t4SZ/yqklg+D9oGM+ILTmBRzIQ2m/xMmsbowiTXymjgVmvrWuc638X6dU2fKJ7As4hxs3rA1BA5sOt0XyqfHQhtYrL/Ovb1iV+C7MRhKicTyoNTc7oVcDDG0VW785d8CPqttDi50w=="
+    bring_your_own_license = true
 
-	database_configuration {
-		admin_password = "Test_String7"
-		backup_destination = "OSS"
-		failover_database = false
-		sid = "ORCL"
-		usable_storage = 15
-	}
+    database_configuration {
+        admin_password = "Test_String7"
+        backup_destination = "OSS"
+        failover_database = false
+        sid = "ORCL"
+        usable_storage = 15
+    }
 
-	backups {
-		cloud_storage_container = "%sacctest-%d"
-		create_if_missing = true
-	}
+    backups {
+        cloud_storage_container = "%sacctest-%d"
+        create_if_missing = true
+    }
 }
 
 resource "oraclepaas_java_service_instance" "test" {
-	name = "tfinstance%d"
-	edition = "SUITE"
-	service_version = "12cRelease212"
-	ssh_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC3QxPp0BFK+ligB9m1FBcFELyvN5EdNUoSwTCe4Zv2b51OIO6wGM/dvTr/yj2ltNA/Vzl9tqf9AUBL8tKjAOk8uukip6G7rfigby+MvoJ9A8N0AC2te3TI+XCfB5Ty2M2OmKJjPOPCd6+OdzhT4cWnPOM+OAiX0DP7WCkO4Kx2kntf8YeTEurTCspOrRjGdo+zZkJxEydMt31asu9zYOTLmZPwLCkhel8vY6SnZhDTNSNkRzxZFv+Mh2VGmqu4SSxfVXr4tcFM6/MbAXlkA8jo+vHpy5sC79T4uNaPu2D8Ed7uC3yDdO3KRVdzZCfWHj4NjixdMs2CtK6EmyeVOPuiYb8/mcTybrb4F/CqA4jydAU6Ok0j0bIqftLyxNgfS31hR1Y3/GNPzly4+uUIgZqmsuVFh5h0L7qc1jMv7wRHphogo5snIp45t9jWNj8uDGzQgWvgbFP5wR7Nt6eS0kaCeGQbxWBDYfjQE801IrwhgMfmdmGw7FFveCH0tFcPm6td/8kMSyg/OewczZN3T62ETQYVsExOxEQl2t4SZ/yqklg+D9oGM+ILTmBRzIQ2m/xMmsbowiTXymjgVmvrWuc638X6dU2fKJ7As4hxs3rA1BA5sOt0XyqfHQhtYrL/Ovb1iV+C7MRhKicTyoNTc7oVcDDG0VW785d8CPqttDi50w=="
-	force_delete = true	
-	bring_your_own_license = true
+    name = "tfinstance%d"
+    edition = "SUITE"
+    service_version = "12cRelease212"
+    ssh_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC3QxPp0BFK+ligB9m1FBcFELyvN5EdNUoSwTCe4Zv2b51OIO6wGM/dvTr/yj2ltNA/Vzl9tqf9AUBL8tKjAOk8uukip6G7rfigby+MvoJ9A8N0AC2te3TI+XCfB5Ty2M2OmKJjPOPCd6+OdzhT4cWnPOM+OAiX0DP7WCkO4Kx2kntf8YeTEurTCspOrRjGdo+zZkJxEydMt31asu9zYOTLmZPwLCkhel8vY6SnZhDTNSNkRzxZFv+Mh2VGmqu4SSxfVXr4tcFM6/MbAXlkA8jo+vHpy5sC79T4uNaPu2D8Ed7uC3yDdO3KRVdzZCfWHj4NjixdMs2CtK6EmyeVOPuiYb8/mcTybrb4F/CqA4jydAU6Ok0j0bIqftLyxNgfS31hR1Y3/GNPzly4+uUIgZqmsuVFh5h0L7qc1jMv7wRHphogo5snIp45t9jWNj8uDGzQgWvgbFP5wR7Nt6eS0kaCeGQbxWBDYfjQE801IrwhgMfmdmGw7FFveCH0tFcPm6td/8kMSyg/OewczZN3T62ETQYVsExOxEQl2t4SZ/yqklg+D9oGM+ILTmBRzIQ2m/xMmsbowiTXymjgVmvrWuc638X6dU2fKJ7As4hxs3rA1BA5sOt0XyqfHQhtYrL/Ovb1iV+C7MRhKicTyoNTc7oVcDDG0VW785d8CPqttDi50w=="
+    force_delete = true	
+    bring_your_own_license = true
 
-	weblogic_server {
-		shape = "oc3"
-		database {
-			name = "${oraclepaas_database_service_instance.test.name}"
-			username = "sys"
-			password = "Test_String7"
-		}
-		admin {
-			username = "terraform-user"
-			password = "Test_String7"
-		}
-	}
+    weblogic_server {
+        shape = "oc3"
+        database {
+            name = "${oraclepaas_database_service_instance.test.name}"
+            username = "sys"
+            password = "Test_String7"
+        }
+        admin {
+            username = "terraform-user"
+            password = "Test_String7"
+        }
+    }
 
-	backups {
-		cloud_storage_container = "%sacctest-%d"
-		auto_generate = true
-	}
+    backups {
+        cloud_storage_container = "%sacctest-%d"
+        auto_generate = true
+    }
 }
 
 resource "oraclepaas_java_access_rule" "test" {
-	name = "test-access-rule-%d"
-	service_instance_id = "${oraclepaas_java_service_instance.test.name}"
-	description = "test-access-rule"
-	ports = "8000"
-	source = "PUBLIC-INTERNET"
-	destination = "WLS_ADMIN"
-	enabled = true
+    name = "test-access-rule-%d"
+    service_instance_id = "${oraclepaas_java_service_instance.test.name}"
+    description = "test-access-rule"
+    ports = "8000"
+    source = "PUBLIC-INTERNET"
+    destination = "WLS_ADMIN"
+    enabled = true
 }
 `, rInt, os.Getenv("OPC_STORAGE_URL"), rInt, rInt, os.Getenv("OPC_STORAGE_URL"), rInt, rInt)
 }
